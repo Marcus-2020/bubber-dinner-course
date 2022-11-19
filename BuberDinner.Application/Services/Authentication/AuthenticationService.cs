c4faaf2d078a5a3351080a4539d86bd5d8ad49f3
Marcus Vinicius Menezes dos Santos <marcus.santos1808@hotmail.com>
2022-11-18 21:21:25 -0300

    feat: implemented the repository pattern for the register and login operations

--- BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,45 +1,70 @@
 using BuberDinner.Application.Common.Interfaces.Authentication;
+using BuberDinner.Application.Common.Interfaces.Persistence;
+using BuberDinner.Domain.Entities;
 
 namespace BuberDinner.Application.Services.Authentication;
 
 public class AuthenticationService : IAuthenticationService
 {
     private readonly IJwtTokenGenerator _jwtTokenGenerator;
+    private readonly IUserRepository _userRepository;
 
-    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator,
+        IUserRepository userRepository)
     {
         _jwtTokenGenerator = jwtTokenGenerator;
+        _userRepository = userRepository;
     }
 
     public AuthenticationResult Register(string firstName, string lastName, 
         string email, string password)
     {
         // Check if the user already exists
+        if (_userRepository.GetUserByEmail(email) is not null)
+        {
+            throw new Exception("User with given email already exists");
+        }
 
+        // Create user (generate unique ID) & persist to DB
+        var user = new User
+        {
+            FirstName = firstName,
+            LastName = lastName,
+            Email = email,
+            Password = password
+        };
 
-        // Create user (generate unique ID)
-        Guid userId = Guid.NewGuid();
+        _userRepository.Add(user);
 
         // Create the JWT token
-        var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+        var token = _jwtTokenGenerator.GenerateToken(user);
 
         return new AuthenticationResult(
-            userId,
-            firstName,
-            lastName, 
-            email,
+            user,
             token
         );
     }
 
     public AuthenticationResult Login(string email, string password)
     {
+        // Check if the user exists
+        if (_userRepository.GetUserByEmail(email) is not User user)
+        {
+            throw new Exception("E-mail or password is incorrect.");
+        }
+
+        // Validate if the password is correct
+        if (user.Password != password)
+        {
+            throw new Exception("E-mail or password is incorrect.");
+        }
+
+        // Create JWT token
+        var token = _jwtTokenGenerator.GenerateToken(user);
+
         return new AuthenticationResult(
-            Guid.NewGuid(),
-            "firstName",
-            "lastName", 
-            email,
-            "token"
+            user,
+            token
         );
     }
 }
---
+++ Docs/DomainModels.md
@@ -0,0 +1,15 @@
+# Domain Models
+
+## User
+
+```json
+{
+  "id": "c06469eb-0a97-4530-83b3-4f72a03ff9e9",
+  "firstName": "Marcus",
+  "lastName": "Santos",
+  "email": "marcus.santos1808@hotmail.com",
+  "password": "Pwd12345."
+}
+```
+
+> Note: Passwords shouldn't be laying around like this ⚠️
